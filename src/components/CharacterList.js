1e45ef0c77d935ec9faf93186e0f9e5e4d12e190
Timothy McGowen <mcgowen17@yahoo.com>
2019-07-26 09:10:53 -0700

    I dont know what isnt working

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]);
+
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
 
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+      .then(e => {
+        setData(e.data.results);
+      })
+      .catch(err => { return ('nothing', err) })
+  }, [data])
+  const tempArray = data.map(e => e)
   return <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <h2>{tempArray.name}</h2>
+  </section>
 
 }
